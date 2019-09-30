1da3d714eac3d27121ac372a7a1ef1c6b0e1b6b0
karmanluo <karmanluo1994@gmail.com>
2019-09-30 23:15:00 +0800

    N62_唯一路径 巧妙的数学方法 加注解

--- LeetcodeAlgorithm/N62_UniquePaths/Solution2.java
+++ LeetcodeAlgorithm/N62_UniquePaths/Solution2.java
@@ -6,10 +6,13 @@ package LeetcodeAlgorithm.N62_UniquePaths;
 * */
 public class Solution2 {
     public int uniquePaths(int m, int n) {
-        int N = m + n - 2;
-        int k = m - 1;
+        int N = m + n - 2;// how much steps we need to do
+        int k = m - 1;// number of steps that need to go down
         long res = 1;
-
+        // here we calculate the total possible path number
+        // Combination(N, k) = n! / (k!(n - k)!)
+        // reduce the numerator and denominator and get
+        // C = ( (n - k + 1) * (n - k + 2) * ... * n ) / k!
         for (int i = 1; i <= k; i++) {
             res = res * (N - k + i) / i;
         }
