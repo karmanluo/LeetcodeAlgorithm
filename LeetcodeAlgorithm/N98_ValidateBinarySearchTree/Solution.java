e8fa39491562bdd423448188e772f4d72a684f97
karmanluo <karmanluo1994@gmail.com>
2019-10-21 19:08:35 +0800

    N98 validate binary search tree--using stack

---
+++ LeetcodeAlgorithm/N98_ValidateBinarySearchTree/Solution.java
@@ -0,0 +1,26 @@
+package LeetcodeAlgorithm.N98_ValidateBinarySearchTree;
+
+import java.util.Stack;
+
+public class Solution {
+    public boolean isValidBST(TreeNode root) {
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode curr = root;
+        TreeNode pre = null;
+
+        while (curr != null || !stack.isEmpty()) {
+            while (curr != null) {
+                stack.push(curr);
+                curr = curr.left;
+            }
+            curr = stack.pop();
+            if (pre != null && curr.val <= pre.val) {
+                return false;
+            } else {
+                pre = curr;
+            }
+            curr = curr.right;
+        }
+        return true;
+    }
+}
