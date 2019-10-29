748f64f96caf63ae0678148d4f8d5b46a3826196
karmanluo <karmanluo1994@gmail.com>
2019-10-29 20:49:23 +0800

    N102_二叉树层次遍历，输出每层的值

---
+++ LeetcodeAlgorithm/N102_BinaryTreeLevelOrderTraversal/TreeNode.java
@@ -0,0 +1,10 @@
+package LeetcodeAlgorithm.N102_BinaryTreeLevelOrderTraversal;
+
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) {
+        val = x;
+    }
+}
