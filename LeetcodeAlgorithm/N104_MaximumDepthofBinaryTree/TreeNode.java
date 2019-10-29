ce462591ee3fa772d8d8a13d4f1786e24470ee8a
karmanluo <karmanluo1994@gmail.com>
2019-10-29 21:14:47 +0800

    N104_二叉树最深的深度

---
+++ LeetcodeAlgorithm/N104_MaximumDepthofBinaryTree/TreeNode.java
@@ -0,0 +1,10 @@
+package LeetcodeAlgorithm.N104_MaximumDepthofBinaryTree;
+
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) {
+        val = x;
+    }
+}
