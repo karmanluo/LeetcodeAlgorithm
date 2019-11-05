01a57d96d2703e199df5011c88b701d193609273
karman <karmanluo1994@gmail.com>
2019-11-06 00:51:04 +0800

    N114_二叉树展开为链表 使用dfs

---
+++ LeetcodeAlgorithm/N114_FlattenBinaryTreetoLinkedList/Solution.java
@@ -0,0 +1,44 @@
+package LeetcodeAlgorithm.N114_FlattenBinaryTreetoLinkedList;
+
+import java.util.Stack;
+
+//Given a binary tree, flatten it to a linked list in-place.
+//
+// For example, given the following tree:
+//    1
+//   / \
+//  2   5
+// / \   \
+//3   4   6
+// The flattened tree should look like:
+//1
+// \
+//  2
+//   \
+//    3
+//     \
+//      4
+//       \
+//        5
+//         \
+//          6
+//
+// Related Topics Tree Depth-first Search
+public class Solution {
+    public void flatten(TreeNode root) {
+        if (root == null) return;
+
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+
+        while (!stack.isEmpty()){
+            TreeNode curr = stack.pop();
+            if (curr.right != null) stack.push(curr.right);
+            if (curr.left != null) stack.push(curr.left);
+            if (!stack.isEmpty()){
+                curr.right = stack.peek();
+            }
+            curr.left = null;
+        }
+    }
+}
---
+++ LeetcodeAlgorithm/N114_FlattenBinaryTreetoLinkedList/TreeNode.java
@@ -0,0 +1,10 @@
+package LeetcodeAlgorithm.N114_FlattenBinaryTreetoLinkedList;
+
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) {
+        val = x;
+    }
+}
