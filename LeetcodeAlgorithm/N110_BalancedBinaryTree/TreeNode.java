f739a9673fa67c104f23be76cd08c21a82d2787d
karman <karmanluo1994@gmail.com>
2019-11-06 01:35:20 +0800

    N110_判断树是否为平衡二叉树 递归

---
+++ LeetcodeAlgorithm/N110_BalancedBinaryTree/TreeNode.java
@@ -0,0 +1,10 @@
+package LeetcodeAlgorithm.N110_BalancedBinaryTree;
+
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) {
+        val = x;
+    }
+}
