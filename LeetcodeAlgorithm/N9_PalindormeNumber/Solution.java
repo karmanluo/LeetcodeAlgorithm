27abd3364f43893d1fcf51c539f49a0f764eb85e
karman <karmanluo1994@gmail.com>
2019-11-07 01:23:52 +0800

    N114_二叉树展开为链表，此种解法是一种递归的解法，并且没有额外占用空间

--- LeetcodeAlgorithm/N9_PalindormeNumber/Solution.java
+++ LeetcodeAlgorithm/N9_PalindormeNumber/Solution.java
@@ -24,7 +24,7 @@ package LeetcodeAlgorithm.N9_PalindormeNumber;
 // Related Topics Math
 class Solution {
     public boolean isPalindrome(int x) {
-        if (x < 0 || x % 10 == 0) return false;
+        if (x < 0 || (x != 0 && x % 10 == 0)) return false;
 
         int rev = 0;
         while (x > rev){
