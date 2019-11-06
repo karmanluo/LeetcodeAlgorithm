1f40864cbdd5494bc5c305e3b1ab57462969eacd
karman <karmanluo1994@gmail.com>
2019-11-07 01:32:16 +0800

    N9_判断整数是否为回文，其实不用担心溢出的问题，因为溢出的情况肯定不是回文。在计算中出现了溢出也不会影响结果

---
+++ LeetcodeAlgorithm/N9_PalindormeNumber/Solution2.java
@@ -0,0 +1,15 @@
+package LeetcodeAlgorithm.N9_PalindormeNumber;
+
+public class Solution2 {
+    public boolean isPalindrome(int x) {
+        if (x < 0) return false;
+
+        int y = x, rev = 0;
+
+        while (y != 0){
+            rev = rev * 10 + y % 10;
+            y /= 10;
+        }
+        return x == rev;
+    }
+}
