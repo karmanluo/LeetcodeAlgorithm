61759c7f76190b65b3577965b66b0d410a61c48f
karmanluo <karmanluo1994@gmail.com>
2019-12-27 19:41:50 +0800

    N122---Best time to Buy and Sell Stock II finished!

--- LeetcodeAlgorithm/N122/BestTimetoBuyandSellStockII/Solution.java
+++ LeetcodeAlgorithm/N122/BestTimetoBuyandSellStockII/Solution.java
@@ -29,5 +29,13 @@ package LeetcodeAlgorithm.N122.BestTimetoBuyandSellStockII;
 public class Solution {
     public int maxProfit(int[] prices) {
         int minPrice = Integer.MAX_VALUE;
+        int maxprofit = 0;
+
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] > prices[i - 1]){
+                maxprofit += prices[i] - prices[i - 1];
+            }
+        }
+        return maxprofit;
     }
 }
