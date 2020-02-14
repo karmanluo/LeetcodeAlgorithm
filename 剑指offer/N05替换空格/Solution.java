96bab94369e850d33a9cd59e281b42e0c8d77012
karmanluo <karmanluo1994@gmail.com>
2020-02-14 22:56:37 +0800

    N05 字符串中替换空格

---
+++ 剑指offer/N05替换空格/Solution.java
@@ -0,0 +1,28 @@
+package 剑指offer.N05替换空格;
+
+/**
+ * 请实现一个函数，把字符串 s 中的每个空格替换成"%20"。
+ *
+ * 示例 1：
+ *
+ * 输入：s = "We are happy."
+ * 输出："We%20are%20happy."
+ *  
+ * 限制：
+ *
+ * 0 <= s 的长度 <= 10000
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/ti-huan-kong-ge-lcof
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class Solution {
+    public String replaceSpace(String s) {
+        StringBuilder res = new StringBuilder();
+        for(Character c : s.toCharArray()){
+            if (c == ' ')   res.append("%20");
+            else res.append(c);
+        }
+        return res.toString();
+    }
+}
