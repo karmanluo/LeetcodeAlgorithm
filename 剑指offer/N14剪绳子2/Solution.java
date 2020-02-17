bf2e3e3e1a0de311b6dbc27177634e875a6202f2
karmanluo <karmanluo1994@gmail.com>
2020-02-17 13:01:53 +0800

    N14 剪绳子对n没有限制了

---
+++ 剑指offer/N14剪绳子2/Solution.java
@@ -0,0 +1,25 @@
+package 剑指offer.N14剪绳子2;
+
+/**
+ *给你一根长度为 n 的绳子，请把绳子剪成整数长度的 m 段（m、n都是整数，n>1并且m>1），每段绳子的长度记为 k[0],k[1]...k[m] 。请问 k[0]*k[1]*...*k[m] 可能的最大乘积是多少？例如，当绳子的长度是8时，我们把它剪成长度分别为2、3、3的三段，此时得到的最大乘积是18。
+ *
+ * 答案需要取模 1e9+7（1000000007），如计算初始结果为：1000000008，请返回 1。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/jian-sheng-zi-ii-lcof
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class Solution {
+    public int cuttingRope(int n) {
+        if(n <= 1) return 0;
+        if (n <= 3) return n - 1;
+
+        int mod = (int)1e9+7;
+        long res = 1;
+        while (n > 4){
+            res = res * 3 % mod;
+            n -= 3;
+        }
+        return (int)(res * n % mod);
+    }
+}
