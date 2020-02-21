f6ebf429553c166ce5cc834a6be8bb08f8de1db8
karmanluo <karmanluo1994@gmail.com>
2020-02-21 23:19:45 +0800

    N464 快速排序算法

--- LeetcodeAlgorithm/N215数组中第K个最大元素/Solution3.java
+++ LeetcodeAlgorithm/N215数组中第K个最大元素/Solution3.java
@@ -7,52 +7,50 @@ import java.util.Random;
  */
 import java.util.Random;
 class Solution3 {
-    int [] nums;
     public int findKthLargest(int[] nums, int k) {
-        this.nums = nums;
         int size = nums.length;
         // kth largest is indexed (N - k)th smallest
-        return quickselect(0, size - 1, size - k);
+        return quickselect(nums ,0, size - 1, size - k);
     }
 
-    public int quickselect(int left, int right, int destPos) {
+    public int quickselect(int[] nums, int left, int right, int destPos) {
         if (left == right)
-            return this.nums[left];
+            return nums[left];
         Random random_num = new Random();
         int pivot_index = left + random_num.nextInt(right - left);
 
-        pivot_index = partition(left, right, pivot_index);
+        pivot_index = partition(nums, left, right, pivot_index);
 
         // the pivot is on (N - k)th smallest position
         if (destPos == pivot_index)
-            return this.nums[destPos];
+            return nums[destPos];
             // go left side
         else if (destPos < pivot_index)
-            return quickselect(left, pivot_index - 1, destPos);
+            return quickselect(nums, left, pivot_index - 1, destPos);
         // go right side
-        return quickselect(pivot_index + 1, right, destPos);
+        return quickselect(nums, pivot_index + 1, right, destPos);
     }
 
-    public void swap(int a, int b) {
-        int tmp = this.nums[a];
-        this.nums[a] = this.nums[b];
-        this.nums[b] = tmp;
+    public void swap(int[] nums,int a, int b) {
+        int tmp = nums[a];
+        nums[a] = nums[b];
+        nums[b] = tmp;
     }
 
-    public int partition(int left, int right, int pivot_index) {
-        int pivot = this.nums[pivot_index];
+    public int partition(int[] nums, int left, int right, int pivot_index) {
+        int pivot = nums[pivot_index];
         // 1. move pivot to end
-        swap(pivot_index, right);
+        swap(nums ,pivot_index, right);
         int store_index = left;
         // 2. move all smaller elements to the left
         for (int i = left; i <= right; i++) {
-            if (this.nums[i] < pivot) {
-                swap(store_index, i);
+            if (nums[i] < pivot) {
+                swap(nums,store_index, i);
                 store_index++;
             }
         }
         // 3. move pivot to its final place
-        swap(store_index, right);
+        swap(nums ,store_index, right);
 
         return store_index;
     }
