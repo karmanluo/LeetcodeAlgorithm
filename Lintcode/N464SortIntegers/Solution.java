e42109a4d402a620bead21550aecdfedefabb384
karmanluo <karmanluo1994@gmail.com>
2020-02-22 15:38:25 +0800

    N464 归并排序

---
+++ Lintcode/N464SortIntegers/Solution.java
@@ -0,0 +1,54 @@
+package Lintcode.N464SortIntegers;
+
+public class Solution {
+    //归并排序
+    public void sortIntegers2(int[] A) {
+        mergeSort(A, 0, A.length - 1);
+    }
+
+    private void mergeSort(int[] nums, int low, int high) {
+        int mid = low + ((high - low) >>> 1);
+
+        if (low < high) {
+            mergeSort(nums, low, mid);
+            mergeSort(nums, mid + 1, high);
+            //合并
+            merge(nums, low, mid, high);
+        }
+    }
+
+    //merge函数实际上是将两个有序数组合并成一个有序数组
+    //因为数组有序，合并很简单，只要维护几个指针就可以了
+    private void merge(int[] arr, int low, int mid, int high) {
+        //temp数组用于暂存合并的结果
+        int[] temp = new int[high - low + 1];
+        //左半边的指针
+        int i = low;
+        //右半边的指针
+        int j = mid+1;
+        //合并后数组的指针
+        int k = 0;
+
+        //将记录由小到大地放进temp数组
+        // 把较小的数先移到新数组中
+        while (i <= mid && j <= high) {
+            if (arr[i] < arr[j]) {
+                temp[k++] = arr[i++];
+            } else {
+                temp[k++] = arr[j++];
+            }
+        }
+
+        //接下来两个while循环是为了将剩余的（比另一边多出来的个数）放到temp数组中
+        while(i <= mid)
+            temp[k++] = arr[i++];
+
+        while(j <= high)
+            temp[k++] = arr[j++];
+
+        //将temp数组中的元素写入到待排数组中
+        for(int l = 0; l < temp.length; l++)
+            arr[low + l] = temp[l];
+
+    }
+}
