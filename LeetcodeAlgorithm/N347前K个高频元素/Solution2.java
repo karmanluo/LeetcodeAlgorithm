a04860f298f026fb5fa4dce6021029284f82ff02
karmanluo <karmanluo1994@gmail.com>
2020-02-23 16:29:04 +0800

    347 桶排序 minHeap

---
+++ LeetcodeAlgorithm/N347前K个高频元素/Solution2.java
@@ -0,0 +1,25 @@
+package LeetcodeAlgorithm.N347前K个高频元素;
+
+import java.util.*;
+
+public class Solution2 {
+    public List<Integer> topKFrequent(int[] nums, int k) {
+        HashMap<Integer, Integer> frequentMap = new HashMap<>();
+        for (int n : nums) {
+            frequentMap.put(n, frequentMap.getOrDefault(n, 0) + 1);
+        }
+        //小根堆好
+        PriorityQueue<Map.Entry<Integer, Integer>> minHeap =
+                new PriorityQueue<>((a, b) -> a.getValue() - b.getValue());
+        for (Map.Entry<Integer, Integer> entry : frequentMap.entrySet()) {
+            minHeap.add(entry);
+            if (minHeap.size() > k) minHeap.poll();
+        }
+
+        List<Integer> res = new ArrayList<>();
+        while (res.size() < k) {
+            res.add(minHeap.poll().getKey());
+        }
+        return res;
+    }
+}
