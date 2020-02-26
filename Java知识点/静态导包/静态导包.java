d942cc5fb86f0e42c1cc1c77101b379a87749dea
karmanluo <karmanluo1994@gmail.com>
2020-02-26 22:21:32 +0800

    静态导包

---
+++ Java知识点/静态导包/静态导包.java
@@ -0,0 +1,11 @@
+package Java知识点.静态导包;
+
+import static java.lang.Math.max;
+
+public class 静态导包 {
+    public static void main(String[] args) {
+
+        int max = max(1,2);
+        System.out.println(max);
+    }
+}
