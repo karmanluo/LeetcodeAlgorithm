2bb0460f0a8c4c0b85c9016652efd7fcaedf2317
karmanluo <karmanluo1994@gmail.com>
2020-03-04 17:53:05 +0800

    排序两个有序的链表

--- LeetcodeAlgorithm/N0___100/N21_MergeTwoSortedLists/Solution.java
+++ LeetcodeAlgorithm/N0___100/N21_MergeTwoSortedLists/Solution.java
@@ -1,21 +1,23 @@
-package LeetcodeAlgorithm.N21_MergeTwoSortedLists;
+package LeetcodeAlgorithm.N0___100.N21_MergeTwoSortedLists;
+
+import 剑指offer.N18删除链表的节点.ListNode;
 
 public class Solution {
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
         ListNode dummy = new ListNode(0);
         ListNode p = l1, q = l2, curr = dummy;
-        while (p != null && q != null){
-            if (p.val <= q.val ){
+        while (p != null && q != null) {
+            if (p.val <= q.val) {
                 curr.next = p;
                 curr = curr.next;
                 p = p.next;
-            }else {
+            } else {
                 curr.next = q;
                 curr = curr.next;
                 q = q.next;
             }
         }
-        curr.next = q == null ? p:q;
+        curr.next = q == null ? p : q;
         return dummy.next;
     }
 }
