b4cb71817cea7d4d057ee81bbeefe236fd291003
karman <karmanluo1994@gmail.com>
2020-05-23 14:35:05 +0800

    最长回文子串

--- LeetcodeAlgorithm/N0___100/N5_LongestPalindromicSubstring/Solution3.java
+++ LeetcodeAlgorithm/N0___100/N5_LongestPalindromicSubstring/Solution3.java
@@ -1,4 +1,4 @@
-package LeetcodeAlgorithm.N5_LongestPalindromicSubstring;
+package LeetcodeAlgorithm.N0___100.N5_LongestPalindromicSubstring;
 
 /*
  * Approach 4: Expand Around Center
@@ -6,32 +6,38 @@ package LeetcodeAlgorithm.N5_LongestPalindromicSubstring;
  * In fact, we could solve it in O(n^2) time using only constant space
  * */
 public class Solution3 {
-    public static String longestPalindrome(String s) {
-        if (s == null || s.length() == 0) return "";
-        int start = 0, end = 0;
+    public String longestPalindrome(String s) {
+        if(s == null || s.length() == 0){
+            return "";
+        }
+
+        char[] charArray = s.toCharArray();
+
+        int maxLen = 0, startIndex = 0;
         for (int i = 0; i < s.length(); i++) {
-            int len1 = expandAroundCenter(s, i, i);
-            int len2 = expandAroundCenter(s, i, i + 1);
-            int len = (len1 > len2) ? len1 : len2;
-            if(len > end - start + 1){
-                start = i - (len - 1) / 2;
-                end = i + len / 2;
+            int len1 = expandCenter(charArray, i, i);
+            int len2 = expandCenter(charArray, i, i + 1);
+
+            int len = Math.max(len1, len2);
+            if(len > maxLen){
+                maxLen = len;
+                startIndex = i - (len - 1) / 2;
             }
         }
-        return s.substring(start, end + 1);
+
+        return s.substring(startIndex, startIndex + maxLen);
     }
 
-    private static int expandAroundCenter(String s, int left, int right) {
-        int L = left, R = right;
-        while (L >= 0 && R < s.length() && s.charAt(L) == s.charAt(R)) {
-            L--;
-            R++;
+    public int expandCenter(char[] charArr, int L, int R){
+        while (L >= 0 && R < charArr.length){
+            if (charArr[L] == charArr[R]){
+                L--;
+                R++;
+            }else {
+                break;
+            }
         }
-        return R - L - 1;
-    }
 
-    public static void main(String[] args) {
-        System.out.println("babad的最长回文为： " + longestPalindrome("babad"));
-        System.out.println("cbbd的最长回文为： " + longestPalindrome("cbbd"));
+        return R - L - 1;
     }
 }
