b3efcdff78b4731c8f118e9e544cde11001aae29
karmanluo <karmanluo1994@gmail.com>
2020-06-11 14:37:11 +0800

    找两个有序数组的中位数

--- src/main/java/LeetcodeAlgorithm/N0___100/N4_MedianOfTheTwoSortedArrays/Solution3.java
+++ src/main/java/LeetcodeAlgorithm/N0___100/N4_MedianOfTheTwoSortedArrays/Solution3.java
@@ -9,37 +9,34 @@ We only need constant memory to store 9 local variables, so the space complexity
  * */
 public class Solution3 {
     public static double findMedianSortedArrays(int[] A, int[] B) {
-        int m = A.length;
-        int n = B.length;
-        if (m > n) { // to ensure m<=n
-            int[] temp = A; A = B; B = temp;
-            int tmp = m; m = n; n = tmp;
-        }
-        int iMin = 0, iMax = m, halfLen = (m + n + 1) / 2;
+        int m = A.length, n = B.length;
+        if (m > n)  return findMedianSortedArrays(B, A);
+
+        int leftTotal = (m + n + 1) / 2;
+        int iMin = 0, iMax = m; // i 在 [0, m]的取值范围
         while (iMin <= iMax) {
             int i = (iMin + iMax) / 2;
-            int j = halfLen - i;
-            if (i < iMax && B[j-1] > A[i]){
-                iMin = i + 1; // i is too small
-            }
-            else if (i > iMin && A[i-1] > B[j]) {
-                iMax = i - 1; // i is too big
-            }
-            else { // i is perfect
+            int j = leftTotal - i;
+            if (j != 0 && i != m && B[j - 1] > A[i]) { // i 需要增大
+                iMin = i + 1;
+            }else if (i != 0 && j != n && A[i - 1] > B[j]) { // i 需要减小
+                iMax = i - 1;
+            }else { //满足要求  或者  进入了边界的情况
                 int maxLeft = 0;
-                if (i == 0) { maxLeft = B[j-1]; }
-                else if (j == 0) { maxLeft = A[i-1]; }
-                else { maxLeft = Math.max(A[i-1], B[j-1]); }
-                if ( (m + n) % 2 == 1 ) { return maxLeft; }
+                if (i == 0) maxLeft = B[j - 1];
+                else if (j == 0) maxLeft = A[i - 1];
+                else maxLeft = Math.max(A[i - 1], B[j - 1]);
+                if ((m + n) % 2 == 1) return maxLeft;
 
                 int minRight = 0;
-                if (i == m) { minRight = B[j]; }
-                else if (j == n) { minRight = A[i]; }
-                else { minRight = Math.min(B[j], A[i]); }
+                if (i == m) minRight = B[j];
+                else if (j == n) minRight = A[i];
+                else minRight = Math.min(A[i], B[j]);
 
                 return (maxLeft + minRight) / 2.0;
             }
         }
+
         return 0.0;
     }
 }
--- 剑指offer题解.md
+++ 剑指offer题解.md
@@ -3487,7 +3487,42 @@ public double findMedianSortedArrays(int[] nums1, int[] nums2) {
     }
 ```
 
+```java
+//关于i进行左右划分
+class Solution {
+    public double findMedianSortedArrays(int[] A, int[] B) {
+        int m = A.length, n = B.length;
+        if (m > n)  return findMedianSortedArrays(B, A);
 
+        int leftTotal = (m + n + 1) / 2;
+        int iMin = 0, iMax = m; // i 在 [0, m]的取值范围
+        while (iMin <= iMax) {
+            int i = (iMin + iMax) / 2;
+            int j = leftTotal - i;
+            if (j != 0 && i != m && B[j - 1] > A[i]) { // i 需要增大
+                iMin = i + 1;
+            }else if (i != 0 && j != n && A[i - 1] > B[j]) { // i 需要减小
+                iMax = i - 1;
+            }else { //满足要求  或者  进入了边界的情况
+                int maxLeft = 0;
+                if (i == 0) maxLeft = B[j - 1];
+                else if (j == 0) maxLeft = A[i - 1];
+                else maxLeft = Math.max(A[i - 1], B[j - 1]);
+                if ((m + n) % 2 == 1) return maxLeft;
+                
+                int minRight = 0;
+                if (i == m) minRight = B[j];
+                else if (j == n) minRight = A[i];
+                else minRight = Math.min(A[i], B[j]);
+                
+                return (maxLeft + minRight) / 2.0;
+            }
+        }
+        
+        return 0.0;
+    }
+}
+```
 
 
 
