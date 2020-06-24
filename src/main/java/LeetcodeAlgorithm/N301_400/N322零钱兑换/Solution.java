cbec6c04c0a05903793b7245c7e0d4e6368b5728
karmanluo <karmanluo1994@gmail.com>
2020-06-24 16:04:19 +0800

    coin-change dfs solution

--- src/main/java/LeetcodeAlgorithm/N301_400/N322零钱兑换/Solution.java
+++ src/main/java/LeetcodeAlgorithm/N301_400/N322零钱兑换/Solution.java
@@ -1,28 +1,30 @@
 package LeetcodeAlgorithm.N301_400.N322零钱兑换;
 
+import java.util.Arrays;
+
 /**
  * @Author:KunmingLuo
  * @Date: 2020/3/8 18:04
  */
 public class Solution {
+    int res = Integer.MAX_VALUE;
     public int coinChange(int[] coins, int amount) {
-        if (amount < 1) return 0;
-        return coinChange(coins, amount, new int[amount + 1]);
+        Arrays.sort(coins);
+        helper(0, amount, coins, coins.length - 1); //尽量使用大额硬币
+
+        return res == Integer.MAX_VALUE ? -1 : res;
     }
 
-    private int coinChange(int[] coins, int rem, int[] count) {
-        if (rem < 0) return -1;
-        if (rem == 0)   return 0;
-
-        if (count[rem] != 0)    return count[rem];
-        int min = Integer.MAX_VALUE;
-        for (int coin : coins){
-            int res = coinChange(coins, rem - coin, count);
-            if (res < min && res >= 0)
-                min = 1 + res;
+    private void helper(int count, int amount, int[] coins, int start) {
+        if (amount == 0) { //找到一种组合
+            if (count < res) res = count;
+            return;
         }
-        count[rem] = min == Integer.MAX_VALUE ? -1 : min;
+        if (start == -1 || amount / coins[start] + count >= res) return;
 
-        return count[rem];
+        for (int j = amount / coins[start]; j >= 0; j--) {
+            helper(count + j, amount - coins[start] * j, coins, start - 1);
+        }
     }
+
 }
