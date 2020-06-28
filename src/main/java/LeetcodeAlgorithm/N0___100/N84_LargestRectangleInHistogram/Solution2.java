5f9e0ff2c26ef53fb131c42d3ad18ea168951643
karmanluo <karmanluo1994@gmail.com>
2020-06-28 21:09:16 +0800

    单调栈 入栈 最大矩形面积

--- src/main/java/LeetcodeAlgorithm/N0___100/N84_LargestRectangleInHistogram/Solution2.java
+++ src/main/java/LeetcodeAlgorithm/N0___100/N84_LargestRectangleInHistogram/Solution2.java
@@ -1,24 +1,27 @@
 package LeetcodeAlgorithm.N0___100.N84_LargestRectangleInHistogram;
 
-import java.util.Stack;
-/*
-* 使用栈的方式，思路比较奇特，不太容易想到
-* */
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+//单调栈 解法
 public class Solution2 {
     public int largestRectangleArea(int[] heights) {
         int len = heights.length;
-        Stack<Integer> s = new Stack<>();
+        Deque<Integer> stack = new ArrayDeque<>();
         int maxArea = 0;
-        for (int i = 0; i <= len; i++) {
-            int h = (i == len ? 0 : heights[i]);
-            if (s.empty() || h >= heights[s.peek()]){
-                s.push(i);
-            }else {
-                int top = s.pop();
-                maxArea = Math.max(maxArea, heights[top] * (s.empty() ? i : (i - 1 - s.peek())));
-                i--;
+
+        int[] newHeights = new int[len + 2];
+        System.arraycopy(heights, 0, newHeights, 1, len);
+
+        for (int i = 0; i < newHeights.length; i++) {
+            while (!stack.isEmpty() && newHeights[i] < newHeights[stack.peek()]) {
+                int h = newHeights[stack.pop()];
+                int w = i - stack.peek() - 1;
+                maxArea = Math.max(maxArea, h * w);
             }
+            stack.push(i);
         }
+
         return maxArea;
     }
 }
