3e2a8b11a767406c5f9f363b00ea520581afdffb
karmanluo <karmanluo1994@gmail.com>
2020-07-03 17:11:38 +0800

    判断二叉树是否对称

--- src/main/java/LeetcodeAlgorithm/N101_200/N101_SymmetricTree/Solution.java
+++ src/main/java/LeetcodeAlgorithm/N101_200/N101_SymmetricTree/Solution.java
@@ -2,12 +2,16 @@ package LeetcodeAlgorithm.N101_200.N101_SymmetricTree;
 
 public class Solution {
     public boolean isSymmetric(TreeNode root) {
-        return root == null || helper(root.left, root.right);
+        if (root == null) return true;
+
+        return isSymmetric(root.left, root.right);
     }
 
-    private boolean helper(TreeNode left, TreeNode right) {
-        if (left == null || right == null) return left == right;
-        if (left.val != right.val) return false;
-        return helper(left.left, right.right) && helper(left.right, right.left);
+    public boolean isSymmetric(TreeNode l, TreeNode r) {
+        if (l == null || r == null) return r == l;
+        if (l.val != r.val) return false;
+
+        return isSymmetric(l.left, r.right) && isSymmetric(l.right, r.left);
     }
+
 }
